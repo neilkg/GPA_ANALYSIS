6aff7575c0ad2f367fa9eb63ff36685e08fa74bd
neilkg <neilkg@umich.edu>
2018-08-18 14:15:33 -0400

    to avoid problems during download

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-mkdir test
-cd test
+mkdir GPX_test
+cd GPX_test
 git init
 git pull https://github.com/neilkg/GPA_ANALYSIS
 make
 mv GPA ~
 cd ..
-mv test/grades.csv Desktop/
-rm -rf test
+mv GPX_test/grades.csv Desktop/
+rm -rf GPX_test
 rm setup.sh
